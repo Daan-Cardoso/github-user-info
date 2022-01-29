c6ed1ce4f99c9eefe3d833f4e0de59c3ca05aca7
Danilo Cardoso <danilo.cardosolima@gmail.com>
2022-01-29 20:36:37 -0300

    feat: add request repositories and starred repositories func

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ import ajax from '@fdaciuk/ajax'
 class App extends Component {
   constructor () {
     super()
+  
+    this.getRepos().bind(this)
+  
     this.state = {
       userInfo: null,
       repos: [],
@@ -21,6 +24,8 @@ class App extends Component {
         repos={this.state.repos}
         starred={this.state.starred}
         handleSearch={(e) => this.handleSearch(e)}
+        getRepos={this.getRepos()}
+        getStarred={this.getRepos('starred')}
       />
     )
   }
@@ -29,9 +34,10 @@ class App extends Component {
     const keyCode = e.which || e.keycode
     const value = e.target.value
     const enter = 13
+    const baseUrl = 'https://api.github.com/users/'
   
     if (keyCode === enter) {
-      ajax().get(`https://api.github.com/users/${value}`)
+      ajax().get(`${baseUrl}${value}`)
         .then(r => this.setState({
           userInfo: {
             username: r.name,
@@ -41,6 +47,26 @@ class App extends Component {
             followers: r.followers,
             following: r.following
           }
+        }))
+    }
+  }
+
+  getRepos (type = 'repos') {
+    return (e) => {
+      const user = this.state.userInfo.login
+      const baseUrl ='https://api.github.com/users/'
+      const repos = type === 'repos' ? '/repos' : ''
+      const starred = type === 'starred' ? '/starred' : ''
+  
+      ajax().get(`${baseUrl}${user}${repos}${starred}`)
+        .then(r => this.setState({
+          [type]: r.map(repo => {
+            return {
+              name: repo.name,
+              link: repo.html_url,
+              id: repo.id
+            }
+          })
         }))
     }
   }
